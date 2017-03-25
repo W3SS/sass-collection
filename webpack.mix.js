07b8c8ff88b9bbecf6b43ef39c298b19a87d6998
Gabriel Burgos <hola@gabrielburgos.cl>
2017-03-25 13:17:14 -0300

    copy files to dist

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,15 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.browserSync('sass-collection.dev')
-	 // Sample asstes
-	 .js('resources/sample/js/main.js', 'public/assets/js/sample.js')
-   .sass('resources/sample/sass/main.scss', 'public/assets/css/sample.css')
-   // Mixins
-   .sass('resources/plugin/sass/main.scss', 'dist/test.css');
+// Copy files to dist
+mix.copy('resources/plugin/sass/', 'dist/', false);
+	 
+// Sample asstes
+mix.js('resources/sample/js/main.js', 'public/assets/js/sample.js');
+mix.sass('resources/sample/sass/main.scss', 'public/assets/css/sample.css')
+
+// Mixins, Functions
+mix.sass('resources/plugin/sass/main.scss', 'dist/test.css');
+
+// Reload
+mix.browserSync('sass-collection.dev');
